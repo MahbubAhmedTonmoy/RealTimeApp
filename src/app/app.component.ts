49d67794723a04614adb9e92c2d789f8bfcbff6c
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2023-08-05 16:31:52 +0600

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { SignalrService } from 'src/service/SignalrService';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RealTimeClient';
+  chartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales: {
+      y: {
+        min: 0
+      }
+    }
+  };
+  chartLabels: string[] = ['Real time data for the chart'];
+  chartType: ChartType = 'bar';
+  chartLegend: boolean = true;
+  constructor(public signalRService: SignalrService, 
+    private http: HttpClient) { }
+  ngOnInit() {
+    this.signalRService.startConnection();
+    this.signalRService.addTransferChartDataListener();   
+    this.startHttpRequest();
+  }
+  
+  private startHttpRequest = () => {
+    debugger
+    this.http.get('https://localhost:5001/api/chart')
+      .subscribe(res => {
+        console.log(res);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
 
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
